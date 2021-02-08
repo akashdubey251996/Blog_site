1a1ce88784e09eadf957fe38ab9d5851c5f85dee
“akashdubey251996@gmail.com” <sanjayss85536@gmail.com>
2021-02-08 18:25:09 +0530

    crispy

--- pro/users/views.py
+++ pro/users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,redirect
 from django.contrib import messages 
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm,UserUpdateForm,ProfileUpdateForm
+from django.contrib.auth.decorators import login_required
 
 def register(request):
      if request.method=='POST':
@@ -16,3 +17,6 @@ def register(request):
 
      return render(request,'users/register.html',{'form':form})
 
+@login_required
+def profile(request):
+     return render(request,'users/profile.html')
